df217f2776e4cfe3bf4ef87f21ea63ced5074acd
luoyecong <luoyec@talkmoney.cn>
2017-11-04 20:28:42 +0800

    async/await的特点

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const Koa = require('koa')
-const app = new Koa()
+function getSyncTime() {
+    return new Promise((resolve, reject) => {
+        try {
+            let startTime = new Date().getTime()
+            setTimeout(() => {
+                let endTime = new Date().getTime()
+                let data = endTime - startTime
+                resolve(data)
+            }, 500)
+        } catch (err) {
+            console.log('never log')
+            reject(err)
+        }
+    })
+}
 
-app.use(async (ctx) => {
-    ctx.body = 'hello koa2'
-})
+async function getSyncData() {
+    let time = await getSyncTime()
+    let data = `endTime - startTime = ${time}`
+    return data
+}
 
-app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+async function getData() {
+    let data = await getSyncData()
+    console.log(data)
+}
+
+console.log(getData())
+
+/**
+ * async/await的特点
+ * 可以让异步逻辑用同步写法实现
+ * 最底层await返回需要是promise对象
+ * 可以通过多层async function的同步写法代替传统的callback嵌套
+ */
