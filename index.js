0764babe9fed5c05751aa78cdaebf81e09f439a7
luoyecong <luoyec@talkmoney.cn>
2017-11-12 20:19:20 +0800

    查询表中所有数据

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "309003",
-    database: "mysql"
+    database: "lyctea_db"
 });
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", function(error, results, fields) {
+connection.query("SELECT * FROM student", function(error, results, fields) {
     if (error) throw error;
-    console.log("The solution is: ", results[0].solution);
+    console.log("The solution is: ", results);
 });
 
 connection.end();
