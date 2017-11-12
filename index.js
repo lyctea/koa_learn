ddf37bd871b77b2e29ca9cfc930495febc99bd9e
luoyecong <luoyec@talkmoney.cn>
2017-11-12 21:14:37 +0800

    连接池会话管理

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "309003",
     database: "lyctea_db"
 });
 
-connection.connect();
+pool.getConnection(function(err, connection) {
+    connection.query("SELECT * FROM student", (err, results, fields) => {
+        //结束会话
+        connection.release();
 
-connection.query("SELECT * FROM student", function(error, results, fields) {
-    if (error) throw error;
-    console.log("The solution is: ", results);
+        //如果有异常就抛出
+        if (err) throw err;
+    });
 });
-
-connection.end();
