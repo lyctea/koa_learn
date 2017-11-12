b9f9cbd20788e41ea07eb94cf2d47fe4ba895679
luoyecong <luoyec@talkmoney.cn>
2017-11-13 00:40:51 +0800

    初始化项目

---
+++ project/config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'koa_demo',
+    USERNAME: 'root',
+    PASSWORD: '309003',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
